9198ef49d6effddcca3bde57184b0ee2ff4a7665
Seersrt <154582792+Seersrt@users.noreply.github.com>
2024-03-06 12:09:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,24 +36,24 @@ ingredients_list = st.multiselect(
 #actually means...
 #if ingredients_list is not null: then do everything below this line that is indented.
 
-if ingredients_list: 
-    ingredients_string = ''
-    name_on_order = title
-    for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ' '
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-      fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+if ingredients_list:
+  ingredients_string = ''
+  name_on_order = title
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 
-    st.write(ingredients_string)
+  st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
+  st.write(my_insert_stmt)
+  time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success("""Your Smoothie is ordered,""" + name_on_order + """!""", icon="✅")
+  if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success("""Your Smoothie is ordered,""" + name_on_order + """!""", icon="✅")
         
