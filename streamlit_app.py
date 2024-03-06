ebb7d5b308e399c31a3abc0bc9f0cf8d614cf3a0
Seersrt <154582792+Seersrt@users.noreply.github.com>
2024-03-06 12:31:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import pandas
+import Pandas
 
 # Write directly to the app
 st.title(":cup_with_straw: My Parents Healthy New Diner :cup_with_straw:")
